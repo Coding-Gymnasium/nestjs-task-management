14b4c8b85cad73c52dd348bdc313c9032148d319
Nico Rithner <nico@altvia.com>
2021-09-08 13:25:43 -0600

    Complete lesson 32 Error handling for getting a non-existing task
    Co-authored-by: Nico nicorithner@gmail.com

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto, GetTasksFilterDto } from './dto';
@@ -33,7 +33,18 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    // try to get task
+    // if not found, throw an error (404 not found)
+    // otherwise, return the found task
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      /** is possible to use a custom message.*/
+      // throw new NotFoundException(`Task with ID ${id} not found`);
+      throw new NotFoundException();
+    }
+
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
