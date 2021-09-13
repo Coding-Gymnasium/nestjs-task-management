619265e17f80faac82fa78c5ea8e45a511e79ba3
Nico Rithner <nicorithner@gmail.com>
2021-09-13 17:48:30 -0600

    Complete create task refactor

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -45,32 +45,10 @@ export class TasksService {
     return found;
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-
-    const task = this.tasksRepository.create({
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    });
-
-    await this.tasksRepository.save(task);
-    return task;
+  createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksRepository.createTask(createTaskDto);
   }
 
-  //  createTask(createTaskDto: CreateTaskDto): Task {
-  //    const { title, description } = createTaskDto;
-  //    const task: Task = {
-  //      id: uuid(),
-  //      title,
-  //      description,
-  //      status: TaskStatus.OPEN,
-  //    };
-  //
-  //    this.tasks.push(task);
-  //    return task;
-  //  }
-  //
   //  updateTaskStatus(id: string, status: TaskStatus) {
   //    const task = this.getTaskById(id);
   //    task.status = status;
